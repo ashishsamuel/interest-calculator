d25a455fa863b55b875445d2c85664bd30d6a0c4
ashishsamuel <ashishsamuelthomson@gmail.com>
2026-03-24 18:20:16 +0530

    validations for the form fields

--- src/App.js
+++ src/App.js
@@ -5,26 +5,66 @@ import { useState } from "react";
 
 function App() {
 
-  const [principalAmount,setPrincipaAmount] = useState("");
-  const [rateOfInterest,setRateOfInterest] = useState("");
-  const [timePeriod,setTimePeriod] = useState("");
+  const [principalAmount,setPrincipaAmount] = useState(0);
+  const [rateOfInterest,setRateOfInterest] = useState(0);
+  const [timePeriod,setTimePeriod] = useState(0);
   const [interest, setInterest] = useState(0);
+  const [principalAmountValid, setPrincipalAmountValid] = useState(true);
+  const [rateOfInteresttValid, setRateOfInterestValid] = useState(true);
+  const [timePeriodValid, setTimePeriodValid] = useState(true);
 
   const calculateInterest = (e)=>{
     e.preventDefault();
-    setInterest((principalAmount*rateOfInterest*timePeriod)/100);
-    setPrincipaAmount("");
-    setRateOfInterest("");
-    setTimePeriod("");
+    if(!principalAmount || !rateOfInterest || !timePeriod)
+      alert("Please fill the form completely")
+    else {
+      setInterest((principalAmount*rateOfInterest*timePeriod)/100);
+      setPrincipaAmount(0);
+      setRateOfInterest(0);
+      setTimePeriod(0);
+    } 
   }
   // console.log("interst value calculated",interest);
 
   const handleReset = (e)=>{
     e.preventDefault();
-    setPrincipaAmount("");
-    setRateOfInterest("");
-    setTimePeriod("");
+    setPrincipaAmount(0);
+    setRateOfInterest(0);
+    setTimePeriod(0);
     setInterest(0);
+    setPrincipalAmountValid(true);
+    setRateOfInterestValid(true);
+    setTimePeriodValid(true);
+  }
+
+  const validateInput = (e)=>{
+    const {name,value} = e.target;
+
+    if(!!value.match(/^[0-9]+$/)){
+      if(name === "pamount") {
+        setPrincipaAmount(value);
+        setPrincipalAmountValid(true);
+      } else if(name === "rateinterest") {
+        setRateOfInterest(value);
+        setRateOfInterestValid(true);
+      } else {
+        setTimePeriod(value);
+        setTimePeriodValid(true);
+      }
+    }
+    else {
+      if(name === "pamount") {
+        setPrincipaAmount(value);
+        setPrincipalAmountValid(false);
+      }  else if(name === "rateinterest") {
+        setRateOfInterest(value);
+        setRateOfInterestValid(false);
+      } else {
+        setTimePeriod(value);
+        setTimePeriodValid(false);
+      }
+    }
+    
   }
   
 
@@ -51,9 +91,13 @@ function App() {
                 variant="outlined"
                 name="pamount"
                 className="w-100 border rounded my-2"
-                value={principalAmount}
-                onChange={(e)=>setPrincipaAmount(e.target.value)}
+                value={principalAmount || ""}
+                onChange={validateInput}
               />
+              {
+                !principalAmountValid && 
+                <small className="text-danger mb-2 fw-bold">* Principle Amount is Invalid</small> 
+              }
 
               <TextField
                 id="rateinterest"
@@ -61,9 +105,14 @@ function App() {
                 variant="outlined"
                 name="rateinterest"
                 className="w-100 border rounded my-2"
-                value={rateOfInterest}
-                onChange={(e)=>setRateOfInterest(e.target.value)}
+                value={rateOfInterest || ""}
+                onChange={validateInput}
               />
+
+              {
+                !rateOfInteresttValid && 
+                <small className="text-danger mb-2 fw-bold">* Rate of Interest is Invalid</small> 
+              }
 
               <TextField
                 id="time"
@@ -71,16 +120,21 @@ function App() {
                 variant="outlined"
                 name="time"
                 className="w-100 border rounded my-2"
-                value={timePeriod}
-                onChange={(e)=>setTimePeriod(e.target.value)}
+                value={timePeriod || ""}
+                onChange={validateInput}
               />
+
+              {
+                !timePeriodValid && 
+                <small className="text-danger mb-2 fw-bold">* Time Period is Invalid</small> 
+              }
             </div>
 
             {/* action buttons */}
             <div className="d-flex justify-content-between">
               <Button
                 className="text-light w-50 bg-dark px-2 m-1 py-3 fw-bold"
-                variant="contained" type="submit"
+                variant="contained" type="submit" disabled={!principalAmount || !principalAmountValid || !rateOfInterest || !rateOfInteresttValid || !timePeriod || !timePeriodValid}
               >
                 Calculate
               </Button>
